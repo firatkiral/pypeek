a04a9d04aeace5387d8f87cfa06201f276c7d498
firat_kiral <firatkiral@gmail.com>
2022-12-20 16:01:34 -0800

    update

--- build-aux/flatpak/pull-aws.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-LOCAL_REPO=${HOME}/flatpak-repos/peek
-REMOTE_REPO=s3://flatpak.uploadedlobster.com
-REGION=eu-central-1
-
-aws s3 sync --region="${REGION}" \
-  "${REMOTE_REPO}" "${LOCAL_REPO}"
