86c4708f7534e89a45ce90780b8550cd9c51a31f
firatkiral <firatkiral@gmail.com>
2023-11-17 20:58:20 -0800

    Update requires-python version to include 3.11

--- src/pypeek/main.py
+++ src/pypeek/main.py
@@ -8,7 +8,7 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 
 user_path, app_path, logger = None, None, None
-__version__ = '2.9.10'
+__version__ = '2.9.12'
 
 def init():
     global user_path, app_path, logger
